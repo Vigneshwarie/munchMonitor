761ae1d055318f1e8281c43e3ed6140a3cbb2851
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-12-30 14:41:25 -0500

    Connected to database using sequelize and app listens after db connection

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS munchmonitor_db;
+CREATE DATABASE munchmonitor_db;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Import the express package
 const express = require('express');
 const path = require('path');
+//Import sequelize package
+const sequelize = require('./config/connection');
 
 //Initialize the package
 const app = express();
@@ -14,6 +16,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3001;
 
 //Listen to the port
-app.listen(PORT, () => {
-     console.log(`Server running on port ${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+     app.listen(PORT, () => {
+          console.log(`Server running on port ${PORT}`);
+     });
 });
