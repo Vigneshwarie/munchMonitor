a5dff5b79c0e74b89a2aa1b7de9b7007b6aabc0d
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-01-05 23:00:15 -0500

    Installed handlebars and imported them in server file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const routes= require('./controllers');
 //Import sequelize package
 const sequelize = require('./config/connection');
 const router = require('./controllers');
+// Importing handlebars 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 //Initialize the package
 const app = express();
@@ -13,6 +16,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Define the port
 const PORT = process.env.PORT || 3001;
