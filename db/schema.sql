f8c4651964b21b39a67cd2b273e999776a880daa
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-01-11 00:34:04 -0500

    Updated schema file for User table

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,10 @@
 DROP DATABASE IF EXISTS munchmonitor_db;
 CREATE DATABASE munchmonitor_db;
+
+CREATE TABLE user (
+     id INTEGER NOT NULL AUTO_INCREMENT Primary Key,
+     first_name VARCHAR(100) NOT NULL,
+     last_name VARCHAR(100) NOT NULL,
+     username VARCHAR(50) NOT NULL,
+     password  NVARCHAR(50) NOT NULL
+);
